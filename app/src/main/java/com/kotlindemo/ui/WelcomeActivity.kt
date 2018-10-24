6fcd1d1e43b48a5e4ed6db28cb141722270fb2ab
Cyy513 <2946000961@qq.com>
2018-10-24 17:17:02 +0800

    登录界面去掉倒计时显示

--- app/src/main/java/com/kotlindemo/ui/WelcomeActivity.kt
+++ app/src/main/java/com/kotlindemo/ui/WelcomeActivity.kt
@@ -3,7 +3,6 @@ package com.kotlindemo.ui
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
 import com.kotlindemo.activity.MainActivity
 import com.kotlindemo.R
 import io.reactivex.Flowable
@@ -15,17 +14,15 @@ import java.util.concurrent.TimeUnit
 class WelcomeActivity : AppCompatActivity() {
 
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
-        //开启3S跳转到主页面线程
         StartThread()
-
     }
 
     private fun StartThread() {
 
-        val timer: TextView = findViewById(R.id.vt_time) //这里的 timer 就是你要控制显示倒计时效果的 TextView
         var mSubscription: Subscription? = null // Subscription 对象，用于取消订阅关系，防止内存泄露
         var count:Long = 4
         Flowable.interval(0, 1, TimeUnit.SECONDS)//设置0延迟，每隔一秒发送一条数据
@@ -37,12 +34,10 @@ class WelcomeActivity : AppCompatActivity() {
                 .observeOn(AndroidSchedulers.mainThread())//操作UI主要在UI线程
                 .subscribe(object : Subscriber<Long> {
                     override fun onSubscribe(s: Subscription?) {
-                        timer.isEnabled = false//在发送数据的时候设置为不能点击
                         mSubscription = s
                         s?.request(Long.MAX_VALUE)//设置请求事件的数量，重要，必须调用
                     }
                     override fun onNext(aLong: Long?) {
-                        timer.text = "${aLong}s" //接受到一条就是会操作一次UI
                     }
                     override fun onComplete() {
                         val intent = Intent()
